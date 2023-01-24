adca48e6f82897de30f77d9d3b3e8eb9de5d358f
gitting-around <gitting-around@users.noreply.github.com>
2023-01-24 14:02:50 +0100

    added index

--- docs/test.py
+++ docs/test.py
@@ -3,5 +3,5 @@ import requests
 
 df = px.data.gapminder().query("country=='Canada'")
 fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
-fig.show()
-fig.write_html(outfile, include_plotlyjs='cdn')
+#fig.show()
+fig.write_html("index.html", include_plotlyjs='cdn')
